1c8fb7cdf9842bfa470e059fa07fe4383f7ac5ef
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-25 23:37:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.length.times{|index|
+  source_array.length.times {|index|
     if !source_array[index]
       return false
     end
