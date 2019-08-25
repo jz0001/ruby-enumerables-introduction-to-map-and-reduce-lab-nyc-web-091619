6b8960220ebbb0952a7279b4fb030d5c71fca175
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-25 23:04:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,10 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  source_array.length.times { |index|
+    source_array[index]=source_array[index]*2
+  }
+  source_array
+end
