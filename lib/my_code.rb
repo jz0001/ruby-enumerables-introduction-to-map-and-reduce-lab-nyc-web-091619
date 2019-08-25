db62b1bd4d260287aea2bfe1e722322ff83fa1fa
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-25 23:30:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,15 +24,8 @@ def map_to_square(source_array)
   source_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   total=starting_point
-  source_array.length.times {|index|
-    total=source_array[index]+total}
-  return total
-end
-
-def reduce_to_total(source_array)
-  total=0
   source_array.length.times {|index|
     total=source_array[index]+total}
   return total
